3141468a4c70ab3572d7407faee8b8346d3c11db
AyanoNK <s.correa.wilches@gmail.com>
2023-04-14 15:56:27 -0500

    format code

--- app/src/main/java/com/example/atlapetesblancae/MainActivity.kt
+++ app/src/main/java/com/example/atlapetesblancae/MainActivity.kt
@@ -109,47 +109,47 @@ class MainActivity : AppCompatActivity() {
         ).setContentValues(contentValues).build()
 
         recording = videoCapture.output.prepareRecording(
-                this, mediaStoreOutputOptions,
-            ).apply {
-                if (PermissionChecker.checkSelfPermission(
-                        this@MainActivity, Manifest.permission.RECORD_AUDIO
-                    ) == PermissionChecker.PERMISSION_GRANTED
-                ) {
-                    withAudioEnabled()
-                }
-            }.start(ContextCompat.getMainExecutor(this)) { recordEvent ->
-                when (recordEvent) {
-                    is VideoRecordEvent.Start -> {
-                        binding.btnTakeRecording.apply {
-                            text = "Parar"
-                            isEnabled = true
-                        }
-                        // Schedule a stop recording task after 10 seconds
-                        Handler(Looper.getMainLooper()).postDelayed({
-                            curRecording?.stop()
-                            curRecording = null
-                        }, 10000L)
-
+            this, mediaStoreOutputOptions,
+        ).apply {
+            if (PermissionChecker.checkSelfPermission(
+                    this@MainActivity, Manifest.permission.RECORD_AUDIO
+                ) == PermissionChecker.PERMISSION_GRANTED
+            ) {
+                withAudioEnabled()
+            }
+        }.start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+            when (recordEvent) {
+                is VideoRecordEvent.Start -> {
+                    binding.btnTakeRecording.apply {
+                        text = "Parar"
+                        isEnabled = true
                     }
-                    is VideoRecordEvent.Finalize -> {
-                        if (!recordEvent.hasError()) {
-                            val msg = "Video saved" + "${recordEvent.outputResults.outputUri}"
-                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                            Log.d(Constants.TAG, msg)
-                        } else {
-                            recording?.close()
-                            recording = null
-                            Log.e(
-                                Constants.TAG, "Video recording failed: " + "${recordEvent.error}"
-                            )
-                        }
-                        binding.btnTakeRecording.apply {
-                            text = getString(R.string.start_capture)
-                            isEnabled = true
-                        }
+                    // Schedule a stop recording task after 10 seconds
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        curRecording?.stop()
+                        curRecording = null
+                    }, 10000L)
+
+                }
+                is VideoRecordEvent.Finalize -> {
+                    if (!recordEvent.hasError()) {
+                        val msg = "Video saved" + "${recordEvent.outputResults.outputUri}"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.d(Constants.TAG, msg)
+                    } else {
+                        recording?.close()
+                        recording = null
+                        Log.e(
+                            Constants.TAG, "Video recording failed: " + "${recordEvent.error}"
+                        )
+                    }
+                    binding.btnTakeRecording.apply {
+                        text = getString(R.string.start_capture)
+                        isEnabled = true
                     }
                 }
             }
+        }
 
 
     }
